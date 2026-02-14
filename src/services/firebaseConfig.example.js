f3718c438126fc6b2f9a14d52059a639a46c7ea2
aayushichhabra <aayushichhabra1010@gmail.com>
2026-02-14 12:55:24 +0530

    Refactor: Use environment variables for API keys

--- src/services/firebaseConfig.example.js
+++ src/services/firebaseConfig.example.js
@@ -4,13 +4,13 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const firebaseConfig = {
-  apiKey: "abc",
-  authDomain: "abc",
-  projectId: "abc",
-  storageBucket: "abc",
-  messagingSenderId: "abc",
-  appId: "abc",
-  measurementId: "abc"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 let app;
